759b8fc24cab461d1d4ce55791c14dcb35d26450
Joseph Mazzini <192231755+J-mazz@users.noreply.github.com>
2025-07-27 16:27:21 -0700

    Delay React render until main CSS loads

--- affiliate-frontend/src/index.js
+++ affiliate-frontend/src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function renderApp() {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+const cssLink = document.querySelector('link[href^="/static/css/"]');
+if (cssLink && !cssLink.sheet) {
+  cssLink.addEventListener('load', renderApp, { once: true });
+} else {
+  renderApp();
+}
