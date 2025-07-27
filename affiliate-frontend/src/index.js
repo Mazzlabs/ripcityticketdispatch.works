44d7349c93fe6a68c2cc83edafa9c162af88ab05
Joseph Mazzini <192231755+J-mazz@users.noreply.github.com>
2025-07-27 16:30:59 -0700

    delay React mount until window load

--- affiliate-frontend/src/index.js
+++ affiliate-frontend/src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function startApp() {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+if (document.readyState === 'complete') {
+  startApp();
+} else {
+  window.addEventListener('load', startApp);
+}
