0470c4a910db5a4b9e89c68259ec694a323017ab
Joseph Mazzini <192231755+J-mazz@users.noreply.github.com>
2025-07-27 19:32:35 -0700

    Rewrite web app with Vite and Tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        blazersRed: '#E03A3E',
+        blazersBlack: '#000000',
+        blazersSilver: '#C4CED4',
+      },
+    },
+  },
+  plugins: [],
+};
